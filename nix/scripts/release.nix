8508c9b5898063e67599028acb91fb75215b5844
csvenke <csvenke@users.noreply.github.com>
2026-02-17 21:52:48 +0100

    ci(release): update release script to prevent duplicate tags

--- nix/scripts/release.nix
+++ nix/scripts/release.nix
@@ -15,8 +15,15 @@ writeShellApplication {
     git
   ];
   text = ''
-    VERSION=$(svu next)
-    git tag "$VERSION"
+    NEXT_TAG=$(svu next)
+
+    if [ -n "$(git tag -l "$NEXT_TAG")" ]; then
+      echo "Tag $NEXT_TAG already exists, skipping release"
+      exit 0
+    fi
+
+    git tag "$NEXT_TAG"
+    git push --tags
     goreleaser release --clean
   '';
 }
